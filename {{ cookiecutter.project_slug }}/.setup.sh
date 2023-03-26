3b19dc70addf59a50b420a51abc2144fd3291e0b
Andrew Powers-Holmes <aholmes@omnom.net>
2023-03-27 10:56:10 +1100

    update gitignore and setup script

--- {{ cookiecutter.project_slug }}/.setup.sh
+++ {{ cookiecutter.project_slug }}/.setup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 echo "Setting up {{ cookiecutter.project_name }} repo in $(pwd)"
 git init
@@ -6,15 +7,33 @@ git add .
 git commit --quiet --message "Initial commit"
 git tag v{{ cookiecutter.project_version }}
 
-echo "Creating python virtualenv"
-/usr/bin/env python3 -m virtualenv .venv
-source .venv/bin/activate
+function fail_and_exit {
+    echo "Failed!"
+    echo 'Please create a venv and install {{ cookiecutter.project_name }} manually.'
+    echo "Cleaning up broken virtualenv..."
+    rm -rf .venv
+    exit 0
+}
 
-echo "Updating pip and setuptools"
-pip install --upgrade --quiet pip setuptools wheel setuptools-scm
+echo -n "Creating python virtualenv... "
+if (/usr/bin/env python3 -m virtualenv .venv); then
+    echo -n "Activating... "
+    source .venv/bin/activate
+    echo "Done."
+else
+    fail_and_exit
+fi
 
-echo "Installing package in dev mode..."
-pip install --editable '.[dev]'
-python -m setuptools_scm
 
+echo -n "Updating pip and setuptools... "
+if (pip install --quiet --require-virtualenv --upgrade pip setuptools wheel setuptools-scm); then
+    echo "Done."
+    echo 'Installing {{ cookiecutter.project_name }} in editable mode...'
+    pip install --editable '.[dev]'
+    python -m setuptools_scm
+else
+    fail_and_exit
+fi
+
+echo "Template deployment for {{ cookiecutter.project_name }} complete!"
 exit 0
