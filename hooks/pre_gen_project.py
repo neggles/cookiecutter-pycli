9a7a03d6e1410d097eee70d8490ef70f2bd22118
Andrew Powers-Holmes <aholmes@omnom.net>
2023-06-01 14:22:41 +1000

    ugh

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,14 +1,11 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 from re import compile as re_compile
 from sys import exit as sys_exit
 
-MODULE_REGEX = re_compile(r"^[_a-zA-Z][_a-zA-Z0-9]+$")
+MODULE_REGEX = re_compile(r"^[_a-zA-Z][\w]+$")
 
-# fmt: off
-module_name = '{{ cookiecutter.__package_name }}'
-# fmt: on
+module_name = r"{{cookiecutter.__package_name}}"
 
 if not MODULE_REGEX.match(module_name):
     print(f"ERROR: {module_name} is not a valid Python module name!")
